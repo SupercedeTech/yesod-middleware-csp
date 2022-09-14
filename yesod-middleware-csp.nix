4222872f760985225ab9462fc7db72a9e5b2fc74
Seva <seva@linux.com>
2022-09-14 14:21:34 +0300

    fix nix build

--- yesod-middleware-csp.nix
+++ yesod-middleware-csp.nix
@@ -1,14 +1,14 @@
 { mkDerivation, base, base64-bytestring, bytestring
 , case-insensitive, classy-prelude, classy-prelude-yesod, conduit
 , containers, directory, fast-logger, filepath, hspec, http-client
-, http-types, monad-logger, network-uri, stdenv, template-haskell
+, http-types, lib, monad-logger, network-uri, template-haskell
 , text, time, uuid, wai-extra, yesod, yesod-core, yesod-static
 , yesod-test
 }:
 mkDerivation {
   pname = "yesod-middleware-csp";
-  version = "0.0.0";
-  src = ./.;
+  version = "1.1.0";
+  sha256 = "1jrfd8r7xvyc556q04qvrib1rwwlsy0rnbc7nxrnqpvn1wv1pb0q";
   libraryHaskellDepends = [
     base base64-bytestring bytestring classy-prelude conduit containers
     directory filepath http-client network-uri template-haskell text
@@ -21,5 +21,5 @@ mkDerivation {
     text uuid wai-extra yesod yesod-core yesod-static yesod-test
   ];
   description = "A middleware for building CSP headers on the fly";
-  license = stdenv.lib.licenses.mit;
+  license = lib.licenses.mit;
 }
